cbf82066509b14c880a1caf6806bd822a7757882
Welander1994 <64834767+Welander1994@users.noreply.github.com>
2022-10-10 09:01:23 +0200

    Update mist.vue

--- src/components/mist.vue
+++ src/components/mist.vue
@@ -15,7 +15,7 @@
     top: 0;
     left: 0;
     width: 100%;
-    height: 120vh;
+    height: 110vh;
     z-index: -99;
     filter: blur(70px);
 }
@@ -23,14 +23,14 @@
 .mist {
     background: linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.3));
     clip-path: polygon(29% 61.3%, 71.3% 38.5%, 100% 50%, 100% 100%, 0% 100%, 0% 42.3%);
-    animation: mist 10s ease-in-out infinite;
+    animation: mist2 10s ease-in-out infinite;
     z-index: -50;
 }
 
 .mist2 {
     background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.3));
     clip-path: polygon(29% 61.3%, 71.3% 38.5%, 100% 50%, 100% 100%, 0% 100%, 0% 42.3%);
-    animation: mist 8s ease-in-out alternate-reverse;
+    animation: mist 8s ease-in-out infinite;
     z-index: -20;
 }
 
@@ -47,4 +47,6 @@
         clip-path: polygon(29% 61.3%, 71.3% 38.5%, 100% 50%, 100% 100%, 0% 100%, 0% 42.3%);
     }
 }
+
+
 </style>
